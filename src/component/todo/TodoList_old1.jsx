942f3e23088c81c4a4bf7c4a660e7fb1d7d2ee10
DESKTOP-T5B94PA\swanb <swanbaek@naver.com>
2024-06-03 18:28:17 +0900

    초간단 ToDoList 앱 추가

---
+++ src/component/todo/TodoList_old1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Stack}  from 'react-bootstrap'
+export default function TodoList({todo}) {
+  return (
+    <Stack>
+      <h1>TodoList</h1>
+
+
+
+      {/* {todo.map((work, i)=>(
+        <h4 key={work.id}>{work.content}</h4>
+      ))} */}
+    </Stack>
+  )
+}
+

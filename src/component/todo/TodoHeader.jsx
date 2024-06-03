abeac0db3bcb9111bfd08180380564c0229f1702
DESKTOP-T5B94PA\swanb <swanbaek@naver.com>
2024-06-03 18:48:08 +0900

    ToDO앱 다시 추가

--- src/component/todo/TodoHeader.jsx
+++ src/component/todo/TodoHeader.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function TodoHeader() {
   return (
-    <div>
-      <h1>TodoHeader</h1>
+    <div className='text-center py-4'>
+      <h1 className='text-secondary my-2'>오늘의 할 일 (To Do List)</h1>
+      <h2 className='text-primary my-2'>{new Date().toDateString()}</h2>
     </div>
   )
 }
--- src/component/todo/TodoListItem.jsx
+++ src/component/todo/TodoListItem.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import './TodoListItem.css'
 
-export default function TodoListItem() {
+export default function TodoListItem(props) {
+    const {id,content,isDone, wdate, onChangeDone, onDelete} =props;
+    const onChangeCheckbox=(e)=>{
+        onChangeDone(id)
+    }
+    const onDeleteHandler =()=>{
+        onDelete(id)
+    }
   return (
-    <div>
-      <h1>TodoListItem</h1>
+    <div className="container TodoListItem">
+      <div className="chkbox">
+        <input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
+      </div>
+      <div className="content">
+        {content}
+      </div>
+      <div className="wdate">
+        {new Date(wdate).toLocaleDateString()}
+      </div>
+      <div className="btDel">
+        <button className="btn btn-sm btn-outline-danger" onClick={onDeleteHandler}> x </button>
+      </div>
     </div>
-  )
-}
+  ) 
+} 
 
